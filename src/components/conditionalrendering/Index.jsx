9cbc8ce28ba324bb2984697561cb729ced9f8394
muradbdvc <muradbdvc@gmail.com>
2024-04-21 19:12:45 +0600

    counter file transfer crash front end

--- src/components/conditionalrendering/Index.jsx
+++ src/components/conditionalrendering/Index.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import HomePage from './HomePage';
 import LoginPage from './LoginPage';
 
+function Count (){
+    const [count, useState] = setState;
+    const increase=()=>{
+        count: count+1;
+    }
+    return({
+        <h1>{count}</h1>
+        <button type='button'></button>
+    })
+}
+export default Count;
 class ConditionalRendering extends Component {
     constructor(props){
         super(props)
