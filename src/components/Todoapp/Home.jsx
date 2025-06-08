0cb89af920701a9b6617f12b44e9c0b7fb285133
muradbdvc <muradbdvc@gmail.com>
2025-06-08 21:53:56 +0600

    Todo home file creat

---
+++ src/components/Todoapp/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const dummyTodos = [
+    {
+        id: 1,
+        title : 'Todo title 1',
+        desc : 'This is desc 1.'
+    },
+    {
+        id: 2,
+        title : 'Todo title 2',
+        desc : 'This is desc 2.'
+    },
+]
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
